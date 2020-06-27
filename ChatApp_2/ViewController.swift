29a82f4f42145bc7b6a0509cd2791f3f84944f61
shutainer <shutainer@yahoo.co.jp>
2020-06-27 19:42:47 +0900

    Initial Commit

---
+++ ChatApp_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp_2
+//
+//  Created by 須藤英隼 on 2020/06/27.
+//  Copyright © 2020 Eishun Sudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
